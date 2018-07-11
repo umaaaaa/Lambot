88638f9dfb9843b4c4708a4f78a962840f65a18c
umaaaaa <k-kurakata@hykwlab.org>
2018-07-11 09:20:20 +0900

    lambot shuffle,choice系コマンドの作成

--- functions/lambot/main.py
+++ functions/lambot/main.py
@@ -3,11 +3,23 @@
 
 from sayings import *
 import random
+import os
+
+
+def check_authorization(token):
+    if token == os.environ['OUTGOING_SLACK_TOKEN']:
+        return
+
+    raise ValueError("token is not authorization")
+
 
 def handle(event, context):
-    text = event['text']
-    cmd_list = text.split()
-    lambot_message = '???';
+    check_authorization(event['token'])
+
+    text           = event['text']
+    cmd_list       = text.split()
+    lambot_message = '';
+
     for i, cmd in enumerate(cmd_list):
         # 最初の1つ目はlambotのはずなのでスキップ
         if (i == 0):
@@ -15,9 +27,20 @@ def handle(event, context):
         if (i == 1):
             if (cmd == 'say'):
                 lambot_message = random.choice(SAYING_LIST)
+                break
+            if (cmd == 'choice'):
+                lambot_message = random.choice(cmd_list[2:])
+                break
+            if (cmd == 'shuffle'):
+                lambot_message = random.shuffle(cmd_list[2:])
+                break
 
     return { 'text': lambot_message }
 
 
 if __name__=='__main__':
-  handle({'text': 'lambot hoge'}, '')
+    value = handle({
+        'text': 'lambot hoge',
+        'token': os.environ['OUTGOING_SLACK_TOKEN']
+    }, '')
+    print value
